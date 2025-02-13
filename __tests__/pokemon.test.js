f8bc68011154f42558ea88d1f948dca6040818ce
kozadireza <hanna.zhamoida1993@gmail.com>
2025-02-13 15:24:33 +0000

    was done task 3

--- __tests__/pokemon.test.js
+++ __tests__/pokemon.test.js
@@ -1,85 +1,202 @@
-const Pokemon = require('../pokemon')
+const { Fire, Water, Grass, Normal, Pokemon } = require("../pokemon.js");
 
-describe.('Pokemon', () => {
-    test('Returns an object with the correct properties', () => {
-        // Assign
-         const expected = "object"
-        // Act
-        const Pikachu = new Pokemon("Pikachu")
-        // Assert
-        expect(typeof Pikachu).toBe(expected)
-    });
-    test('Returns name property of the object', () => {
-        // Assign
-         const expected = "Pikachu"
-        // Act
-        const Pikachu = new Pokemon("Pikachu")
-        // Assert
-        expect(Pikachu.name).toBe(expected)
-    });
-describe('take_damage', () => {
-test('Reduce the value of property hitpoints by the amount of damage',() => {
-// Assign 
-const amtOfdamage = 5
+describe("Pokemon", () => {
+  test("Returns an object with the correct properties", () => {
+    // Assign
+    const expected = "object";
+    // Act
+    const Pikachu = new Pokemon("Pikachu");
+    // Assert
+    expect(typeof Pikachu).toBe(expected);
+  });
+  test("Returns name property of the object", () => {
+    // Assign
+    const expected = "Pikachu";
+    // Act
+    const Pikachu = new Pokemon("Pikachu");
+    // Assert
+    expect(Pikachu.name).toBe(expected);
+  });
+  describe("take_damage", () => {
+    test("Reduce the value of property hitpoints by the amount of damage", () => {
+      // Assign
+      const amtOfdamage = 5;
 
-//Act 
-const Pikachu = new Pokemon("Pikachu")
-Pikachu.take_damage(amtOfdamage)
-//Assert
-expect(Pikachu.hitPoints).toBe(95)
-})
-// test.todo(' ')
+      //Act
+      const Pikachu = new Pokemon("Pikachu");
+      Pikachu.take_damage(amtOfdamage);
+      //Assert
+      expect(Pikachu.hitPoints).toBe(95);
+    });
+    // test.todo(' ')
+  });
+
+  describe("useMove", () => {
+    test("Reduce the value of property attackdamage by the usedEnergy", () => {
+      // Assign
+      const usedEnergy = 10;
+
+      //Act
+      const Pikachu = new Pokemon("Pikachu");
+      Pikachu.useMove(usedEnergy);
+      //Assert
+      expect(Pikachu.attack_damage).toBe(90);
+    });
+
+    test("Check that message was shown after Pokemon1 has attacked other Pokemon2", () => {
+      // Assign
+      const usedEnergy = 10;
+
+      //Act
+      const Pikachu = new Pokemon("Pikachu");
+      const spy = jest.spyOn(console, "log"); // called before function is invoked
+      Pikachu.useMove(usedEnergy);
+      //Assert
+
+      expect(spy.mock.calls.length).toBe(1); //returns array which shows how many times it was called
+    });
+
+    test("Check that specified message was shown after Pokemon1 has attacked other Pokemon2", () => {
+      // Assign
+      const usedEnergy = 10;
+
+      //Act
+      const Pikachu = new Pokemon("Pikachu");
+      const spy = jest.spyOn(console, "log"); // called before function is invoked
+      Pikachu.useMove(usedEnergy);
+      //Assert
+
+      expect(spy).toHaveBeenCalledWith("PokemonX used PokemonX's move"); //returns array which shows how many times it was called
+    });
+  });
+  describe("hasFainted", () => {
+    test("Should return true if hitpoints property reaached 0", () => {
+      // Assign
+
+      //Act
+      const Pikachu = new Pokemon("Pikachu");
+      Pikachu.take_damage(100);
+      const result = Pikachu.hasFainted();
+
+      //Assert
+      expect(result).toBe(true);
+    });
+
+    test("Should return false if hitpoints property has NOT reaached 0", () => {
+      // Assign
+
+      //Act
+      const Pikachu = new Pokemon("Pikachu");
+      Pikachu.take_damage(50);
+      const result = Pikachu.hasFainted();
+
+      //Assert
+      expect(result).toBe(false);
+    });
+  });
 });
 
-describe('useMove', () => {
-    test('Reduce the value of property attackdamage by the usedEnergy',() => {
-    // Assign 
-    const usedEnergy = 10
-    
-    //Act 
-    const Pikachu = new Pokemon("Pikachu")
-    Pikachu.useMove(usedEnergy)
-    //Assert
-    expect(Pikachu.attack_damage).toBe(90)
-    })
-    
-
-    test('Check that message was shown after Pokemon1 has attacked other Pokemon2',() => {
-        // Assign 
-        const usedEnergy = 10
-        
-        //Act 
-        const Pikachu = new Pokemon("Pikachu")
-        const spy = jest.spyOn(console, "log") // called before function is invoked
-        Pikachu.useMove(usedEnergy)
-        //Assert
-        
-        expect(spy.mock.calls.length).toBe(1) //returns array which shows how many times it was called
-        })
+describe("ClassesTypes: isEffectiveAgainst", () => {
+  describe("Water", () => {
+    test("returns True if given Pokemon2 of Fire Type", () => {
+      const Pokemon2 = new Fire("Charmander", "fire");
+      const Pokemon1 = new Water("Squirtle", "water");
+      const result = Pokemon1.isEffectiveAgainst(Pokemon2);
+      //Assert
+      expect(result).toBe(true);
     });
-describe('hasFainted', () => {
-        test('Should return true if hitpoints property reaached 0',() => {
-        // Assign 
+    test("returns false if given Pokemon2 of Grass Type", () => {
+      const Pokemon2 = new Grass("Bulbasaur", "grass");
+      const Pokemon1 = new Water("Squirtle", "water");
+      const result = Pokemon1.isEffectiveAgainst(Pokemon2);
+      //Assert
+      expect(result).toBe(false);
+    });
+  });
+  describe("Fire", () => {
+    test("returns True if given Pokemon2 of Grass Type", () => {
+      const Pokemon2 = new Grass("Leafeon", "grass");
+      const Pokemon1 = new Fire("Charmander", "fire");
+      const result = Pokemon1.isEffectiveAgainst(Pokemon2);
+      //Assert
+      expect(result).toBe(true);
+    });
+    test("returns false if given Pokemon2 of Grass Type", () => {
+      const Pokemon2 = new Water("Squirtle", "water");
+      const Pokemon1 = new Fire("Charmander", "fire");
+      const result = Pokemon1.isEffectiveAgainst(Pokemon2);
+      //Assert
+      expect(result).toBe(false);
+    });
+  });
 
-        //Act 
-        const Pikachu = new Pokemon("Pikachu")
-        Pikachu.take_damage(100)
-        const result = Pikachu.hasFainted()
-        
+  describe("Grass", () => {
+    test("returns True if given Pokemon2 of Water Type", () => {
+      const Pokemon1 = new Grass("Leafeon", "grass");
+      const Pokemon2 = new Water("Vaporeon", "water");
+      const result = Pokemon1.isEffectiveAgainst(Pokemon2);
+      //Assert
+      expect(result).toBe(true);
+    });
+    test("returns false if given Pokemon2 of Grass Type", () => {
+      const Pokemon1 = new Grass("Leafeon", "grass");
+      const Pokemon2 = new Fire("Charmander", "fire");
+      const result = Pokemon1.isEffectiveAgainst(Pokemon2);
+      //Assert
+      expect(result).toBe(false);
+    });
+  });
+
+  describe("ClassesTypes: isWeakTo", () => {
+    describe("Water", () => {
+      test("returns True if given Pokemon2 of Grass Type", () => {
+        const Pokemon2 = new Grass("Charmander", "grass");
+        const Pokemon1 = new Water("Squirtle", "water");
+        const result = Pokemon1.isWeakTo(Pokemon2);
         //Assert
-        expect(result).toBe(true)
-        })
+        expect(result).toBe(true);
+      });
+      test("returns False if given Pokemon2 of Fire Type", () => {
+        const Pokemon2 = new Fire("Charmander", "fire");
+        const Pokemon1 = new Water("Squirtle", "water");
+        const result = Pokemon1.isWeakTo(Pokemon2);
+        //Assert
+        expect(result).toBe(false);
+      });
+    });
 
-        test('Should return false if hitpoints property has NOT reaached 0',() => {
-            // Assign 
-    
-            //Act 
-            const Pikachu = new Pokemon("Pikachu")
-            Pikachu.take_damage(50)
-            const result = Pikachu.hasFainted()
-            
-            //Assert
-            expect(result).toBe(false)
-            })
-})
-})
+    describe("Grass", () => {
+      test("returns True if given Pokemon2 of Grass Type", () => {
+        const Pokemon2 = new Fire("Charmander", "fire");
+        const Pokemon1 = new Grass("Charmander", "grass");
+        const result = Pokemon1.isWeakTo(Pokemon2);
+        //Assert
+        expect(result).toBe(true);
+      });
+      test("returns false if given Pokemon2 of Water Type", () => {
+        const Pokemon1 = new Grass("Charmander", "grass");
+        const Pokemon2 = new Water("Squirtle", "water");
+        const result = Pokemon1.isWeakTo(Pokemon2);
+        //Assert
+        expect(result).toBe(false);
+      });
+    });
+
+    describe("Fire", () => {
+      test("returns True if given Pokemon2 of Water Type", () => {
+        const Pokemon2 = new Water("Squirtle", "water");
+        const Pokemon1 = new Fire("Charmander", "fire");
+        const result = Pokemon1.isWeakTo(Pokemon2);
+        //Assert
+        expect(result).toBe(true);
+      });
+      test("returns False if given Pokemon2 of Water Type", () => {
+        const Pokemon2 = new Grass("Charmander", "grass");
+        const Pokemon1 = new Fire("Charmander", "fire");
+        const result = Pokemon1.isWeakTo(Pokemon2);
+        //Assert
+        expect(result).toBe(false);
+      });
+    });
+  });
+});
--- pokemon.js
+++ pokemon.js
@@ -1,30 +1,117 @@
 class Pokemon {
-    constructor(name) {
-        this.name = name
-        this.hitPoints = 100 //health
-        this.attack_damage = 100 //energy expended 
-        this.move = "tackle"
-        
-    }
+  constructor(name) {
+    this.name = name;
+    this.hitPoints = 100; //health
+    this.attack_damage = 100; //energy expended
+    this.move = "tackle";
+  }
 
-    take_damage(amtOfdamage) {
-        this.hitPoints -= amtOfdamage
-    }
+  take_damage(amtOfdamage) {
+    this.hitPoints -= amtOfdamage;
+  }
 
-    useMove(usedEnergy) {
-        this.attack_damage -= usedEnergy
-        console.log("PokemonX used PokemonX's move")
-        
-    }
+  useMove(usedEnergy) {
+    this.attack_damage -= usedEnergy;
+    console.log("PokemonX used PokemonX's move");
+  }
 
-    hasFainted() {
-        if(this.hitPoints === 0) {
-            return true
-        }
-        return false
+  hasFainted() {
+    if (this.hitPoints === 0) {
+      return true;
     }
+    return false;
+  }
 }
-const Pikachu = new Pokemon("Pikachu")
 
+class Fire extends Pokemon {
+  constructor(name, type) {
+    super(name);
+    this.type = type;
+  }
+  isEffectiveAgainst(Pokemon2) {
+    if (Pokemon2.type === "grass") {
+      return true;
+    }
+    if (Pokemon2.type === "water") {
+      return false;
+    }
+  }
+  isWeakTo(Pokemon2) {
+    if (Pokemon2.type === "water") {
+      return true;
+    }
+    if (Pokemon2.type === "grass") {
+      return false;
+    }
+  }
+}
 
-module.exports = Pokemon
+class Water extends Pokemon {
+  constructor(name, type) {
+    super(name);
+    this.type = type;
+  }
+  isEffectiveAgainst(Pokemon2) {
+    if (Pokemon2.type === "fire") {
+      return true;
+    }
+    if (Pokemon2.type === "grass") {
+      return false;
+    }
+  }
+
+  isWeakTo(Pokemon2) {
+    if (Pokemon2.type === "grass") {
+      return true;
+    }
+    if (Pokemon2.type === "fire") {
+      return false;
+    }
+  }
+}
+
+class Grass extends Pokemon {
+  constructor(name, type) {
+    super(name);
+    this.type = type;
+  }
+  isEffectiveAgainst(Pokemon2) {
+    if (Pokemon2.type === "water") {
+      return true;
+    }
+    if (Pokemon2.type === "fire") {
+      return false;
+    }
+  }
+  isWeakTo(Pokemon2) {
+    if (Pokemon2.type === "fire") {
+      return true;
+    }
+    if (Pokemon2.type === "water") {
+      return false;
+    }
+  }
+}
+
+class Normal extends Pokemon {
+  constructor(name, type) {
+    super(name);
+    this.type = type;
+  }
+  isEffectiveAgainst(Pokemon2) {
+    return null; /////check later
+  }
+
+  isWeakTo(Pokemon2) {
+    return null; /////check later
+  }
+}
+// const Pikachu = new Pokemon("Pikachu");
+
+// const Pokemon2 = new Grass("Leafeon", "grass");
+// const Pokemon1 = new Fire("Charmander", "fire");
+// console.log(Pokemon1);
+// console.log(Pokemon2);
+// Pokemon1.isEffectiveAgainst(Pokemon2);
+module.exports = { Pokemon, Fire, Water, Normal, Grass };
+// module.exports = Water;
